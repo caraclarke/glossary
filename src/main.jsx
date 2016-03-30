43974eb86098254cad9843324513e1f6eadbe2d5
caraclarke <cara.clarke8@gmail.com>
2016-03-30 16:21:25 -0400

    Delete from main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Routes = require('./routes.jsx');
 var BasePage = require('./components/BasePage.jsx');
 
 ReactDOM.render(<BasePage bgColor="#fff" titleColor="#3097d1" linkColor="" />, document.getElementById('gloss'));
