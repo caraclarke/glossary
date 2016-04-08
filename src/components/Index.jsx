d802ce19752085582e48100fdd4c1de37373fca6
caraclarke <cara.clarke8@gmail.com>
2016-04-08 12:32:15 -0400

    Changed regex to fix duplicate id issue

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -27,7 +27,8 @@ var Index = React.createClass({
       var seeAlsoArray = seeAlsoReplace.replace(/(\s\(.+\))+/g, '').split(', ');
       
       // get rid of any parenthesis for the id, get rid of spaces, turn lowercase
-      var newTextId = data.title.$t.replace(/(\s\(.+\))+/g, '').replace(/\W+/g, '').split(' ').join('').toLowerCase();
+      var newTextId = data.title.$t.replace(/(\s\()/g, '').replace(/(\))/g, '').replace(/\W+/g, '').split(' ').join('').toLowerCase();
+      console.log(newTextId);
       
       // test if the indicator for a see also term appears in the text
       if ((/(\[\[Glossary:\s)/g).test(data.content.$t) == true) {
